2e63898f2b2c1fa0287ad47cf7a13e548e694178
Andy <andy@quietcode.org>
2026-06-01 20:37:30 +0200

    Open database through native file panel

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -4,14 +4,28 @@
 #include "SchemaReader.h"
 
 #include <Application.h>
+#include <Entry.h>
+#include <FilePanel.h>
 #include <GroupLayout.h>
+#include <ListItem.h>
 #include <ListView.h>
+#include <Menu.h>
+#include <MenuBar.h>
+#include <MenuItem.h>
+#include <Messenger.h>
+#include <Node.h>
+#include <Path.h>
 #include <ScrollView.h>
 #include <StringItem.h>
 #include <StringView.h>
 
 #include <string>
 #include <vector>
+
+enum {
+	kMessageOpenDatabase = 'opdb',
+	kMessageDatabaseSelected = 'dbsl'
+};
 
 MainWindow::MainWindow(const char* databasePath)
 	:
@@ -20,9 +34,16 @@ MainWindow::MainWindow(const char* databasePath)
 		B_TITLED_WINDOW,
 		B_ASYNCHRONOUS_CONTROLS | B_QUIT_ON_WINDOW_CLOSE),
 	fStatusView(NULL),
-	fTableListView(NULL)
+	fTableListView(NULL),
+	fOpenPanel(NULL)
 {
 	SetLayout(new BGroupLayout(B_VERTICAL));
+
+	BMenuBar* menuBar = new BMenuBar("menuBar");
+	BMenu* fileMenu = new BMenu("File");
+	fileMenu->AddItem(new BMenuItem("Open...", new BMessage(kMessageOpenDatabase), 'O'));
+	menuBar->AddItem(fileMenu);
+	AddChild(menuBar);
 
 	fStatusView = new BStringView("status",
 		"BeSqlitEd native Haiku skeleton");
@@ -39,6 +60,43 @@ MainWindow::MainWindow(const char* databasePath)
 	LoadDatabase(databasePath);
 }
 
+MainWindow::~MainWindow()
+{
+	delete fOpenPanel;
+}
+
+void
+MainWindow::MessageReceived(BMessage* message)
+{
+	switch (message->what) {
+		case kMessageOpenDatabase:
+			ShowOpenPanel();
+			break;
+
+		case kMessageDatabaseSelected:
+		{
+			entry_ref ref;
+			if (message->FindRef("refs", &ref) != B_OK) {
+				fStatusView->SetText("No database file selected.");
+				break;
+			}
+
+			BPath path(&ref);
+			if (path.InitCheck() != B_OK) {
+				fStatusView->SetText("Could not resolve selected database path.");
+				break;
+			}
+
+			LoadDatabase(path.Path());
+			break;
+		}
+
+		default:
+			BWindow::MessageReceived(message);
+			break;
+	}
+}
+
 bool
 MainWindow::QuitRequested()
 {
@@ -47,10 +105,28 @@ MainWindow::QuitRequested()
 }
 
 void
+MainWindow::ShowOpenPanel()
+{
+	if (fOpenPanel == NULL) {
+		BMessenger target(this);
+		fOpenPanel = new BFilePanel(B_OPEN_PANEL,
+			&target,
+			NULL,
+			B_FILE_NODE,
+			false,
+			new BMessage(kMessageDatabaseSelected));
+	}
+
+	fOpenPanel->Show();
+}
+
+void
 MainWindow::LoadDatabase(const char* path)
 {
+	ClearTableList();
+
 	if (path == NULL || path[0] == '\0') {
-		fStatusView->SetText("No database path supplied. Start with: BeSqlitEd <database>");
+		fStatusView->SetText("No database open. Choose File > Open...");
 		return;
 	}
 
@@ -86,4 +162,11 @@ MainWindow::LoadDatabase(const char* path)
 			it != tables.end(); ++it) {
 		fTableListView->AddItem(new BStringItem(it->c_str()));
 	}
+}
+
+void
+MainWindow::ClearTableList()
+{
+	while (fTableListView->CountItems() > 0)
+		delete fTableListView->RemoveItem((int32)0);
 }
